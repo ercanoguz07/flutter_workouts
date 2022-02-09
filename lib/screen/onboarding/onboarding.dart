2a23c6dde2dd21a7610e16b21ba60e06f3dbe537
ercanoguz07 <52664615+ercanoguz07@users.noreply.github.com>
2022-02-09 09:21:05 +0300

    flutter main

--- lib/screen/onboarding/onboarding.dart
+++ lib/screen/onboarding/onboarding.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_workouts/screen/home/homescreen.dart';
+import 'package:flutter_workouts/screen/login/loginscreen.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import '../../constants.dart';
 import 'model/onboardingmodel.dart';
@@ -51,7 +52,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
-        builder: (context) => HomeScreen(),
+        builder: (context) => LoginScreen(),
       ),
     );
   }
